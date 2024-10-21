70d44960d71f2529d77ee9fcdbd809d4c0a7816f
lawlessowl <erokhin-maksim@bk.ru>
2024-10-21 20:59:06 +0300

    feat: added audio track

--- index.js
+++ index.js
@@ -4,14 +4,18 @@ const musicList = [
         title: "OP",
         tracks: [
             {
-                artist: "KISIDA KYODAN & THE AKEBOSI ROCKETS",
-                title: "HIGHSCHOOL OF THE DEAD",
-                isHot: true
-            },
-            {
                 artist: "ALI",
                 title: "LOST IN PARADISE",
-                isHot: true
+                isHot: true,
+                imageSource: "/musicPlayer-/src/img/lostInParadise.jpg",
+                songSource: "./src/songs/Ali Lost In Paradise (Jujutsu Kaisen Ending Theme Song) (Feat. Aklo).mp3"
+            },
+            {
+                artist: "Kenshi Yonezu",
+                title: "KICK BACK",
+                isHot: true,
+                imageSource: "/musicPlayer-/src/img/kickBack.jpg",
+                songSource: "./src/songs/kickBack.mp3"
             }
         ]
     },
@@ -22,12 +26,16 @@ const musicList = [
             {
                 artist: "Dreamhop",
                 title: "Somewhere Elsewhere",
-                isHot: true
+                isHot: true,
+                imageSource: "/musicPlayer-/src/img/somewhereElsewhere.jpg",
+                songSource: "./src/songs/SomewhereElsewhere.mp3"
             },
             {
                 artist: "DOMO",
                 title: "Alone",
-                isHot: true
+                isHot: true,
+                imageSource: "/musicPlayer-/src/img/alone.jpg",
+                songSource: "./src/songs/alone.mp3"
             }
         ]
     }
@@ -42,19 +50,21 @@ for (let i = 0; i < musicList.length; i++) {
 
     const playListTitleElement = document.createElement('h2');
     playListTitleElement.textContent = playList.title;
-    playListElement.appendChild(playListTitleElement);
 
     const tracksElement = document.createElement("ul");
 
     for (let j = 0; j < playList.tracks.length; j++) {
         const track = playList.tracks[j];
         const trackElement = document.createElement('li');
-        const trackTitleElement = document.createElement('p');
-        trackTitleElement.textContent = track.title;
-        trackElement.appendChild(trackTitleElement);
-        tracksElement.appendChild(trackElement);
+        trackImgElement = document.createElement('img');
+        trackImgElement.src = track.imageSource;
+        trackSongElement = document.createElement('audio');
+        trackSongElement.setAttribute('controls', '');
+        trackSongElement.src = track.songSource;
+        trackElement.append(trackImgElement, track.title, trackSongElement);
+        tracksElement.append(trackElement);
     }
 
-    playListElement.appendChild(tracksElement);
-    rootRender.appendChild(playListElement);
+    playListElement.append(playListTitleElement, tracksElement); 
+    rootRender.append(playListElement);
 }
